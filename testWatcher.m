a83c134876220d322e472b2771e29dbfd4068de0
efunn <e@ethanoblak.com>
2016-07-22 16:56:16 -0500

    working, blocking watcher (not async)

--- testWatcher.m
+++ testWatcher.m
@@ -1,14 +1,20 @@
 classdef testWatcher < handle
     properties
-    watch_dir = '/Users/efun/seqlearn/bold/split/'
-    file_ext = '*.nii'
-    % file_ext = '.imgdat'
-    read_files = {}
+        watch_dir = '~/code/instabrain/data/'
+        file_ext = '*.imgdat'
+        match_dir
+        read_files = {}
+        in_process_files = {}
+        % pool = gcp()
+        in_process_slices = []
+        in_process_volumes = []
+        fmri_data = [] % (rep,slice,value)
+        slice_dim = 96 % or 64, usually
     end
 
     methods
         function self = testWatcher
-            % initialization here
+            self.match_dir = strcat(self.watch_dir,self.file_ext);
         end
 
         function run(self)
@@ -17,25 +23,36 @@ classdef testWatcher < handle
             end
         end
 
-        function check_dir(self)
-            file_list = dir(strcat(self.watch_dir,self.file_ext));
-            file_names = {};
-            for index = 1:size(file_list,1)
-                file_names{size(file_names,2)+1} = file_list(index).name;
+        function checkDir(self)
+            all_files = dir(self.match_dir);
+            all_files = {all_files.name};
+            to_process_list = all_files(not(ismember(all_files,self.in_process_files)));
+            for slice = to_process_list
+                slice = char(slice);
+                rep_num = str2num(cell2mat(regexp(slice,'(?<=-R).*(?=-E)','match')));
+                slice_num = str2num(cell2mat(regexp(slice,'(?<=-S).*(?=.imgdat)','match')));
+                self.fmri_data(rep_num,slice_num,:) = self.readSlice(strcat(self.watch_dir,slice));
+                %    - add to in_process list
+                %    - start processing
             end
-            self.read_files = file_names;
         end
 
-        function check_dir_pseudocode(self)
-            % 1. get list of .imgdat files in watch_dir
-            % 2. compare to in_process files in watch_dir
-            % 3. for each file, if not in in_process list:
-            %    - add to in_process list
-            %    - start processing
-            % 4. after processing:
-            %    - move to watch_dir/processed
-            %    - remove from in_process list
-            %    - increment dict/counter
+        function showSlice(self, rep, slice)
+            vis_slice = reshape(self.fmri_data(rep,slice,:),self.slice_dim,self.slice_dim);
+            vis_slice = vis_slice/max(max(vis_slice));
+            imshow(vis_slice);
         end
+    end
+
+    methods(Static)
+        function slice_data = readSlice(file_name)
+            f = fopen(file_name);
+            slice_data = fread(f,'uint16');
+            fclose(f);
+        end
+        % 4. after processing:
+        %    - move to watch_dir/processed
+        %    - remove from in_process list
+        %    - increment dict/counter
     end
 end
