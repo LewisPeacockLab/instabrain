863fa194746d0b86f559addfc4d60bac816116c2
efunn <e@ethanoblak.com>
2016-07-25 17:56:15 -0500

    testing parallel processing

--- instaLocalizer.m
+++ instaLocalizer.m
@@ -1,9 +1,16 @@
 % stage 1:
 % a.) freesurfer recon-all
 % b.) spm preprocessing
-%     - alignment (anatomical<->func)
-%     - motion correction (func<->func)
+%     - alignment (anatomical<->func with flirt -bbr)
+%     - motion correction (func<->func with mcflirt)
 
 % stage 2: 
 % a.) freesurfer ROI selection
 % b.) transform ROIs to RFI space
+
+% stage 3:
+% model generation
+% SPM t-stats map
+
+% stage 4:
+% classifier generation
--- testWatcher.m
+++ testWatcher.m
@@ -1,6 +1,7 @@
 classdef testWatcher < handle
     properties
-        watch_dir = '~/Dropbox/code/instabrain/data/'
+        % watch_dir = '~/Dropbox/code/instabrain/data/'
+        watch_dir = '~/code/instabrain/data/'
         file_ext = '*.imgdat'
         archive_dir
         match_dir
@@ -13,18 +14,20 @@ classdef testWatcher < handle
         fmri_data_mc = []
         roi_data_zscore = []
         fmri_data_raw_status = [] % (rep,slice)
-        fmri_data_mc_status = [] % (rep,slice)
         mc_proc_handles = []
         slice_dim = 96 % depends on sequence
         num_slices = 64 % depends on sequence
         volumes_per_run = 50 % depends on protocol
         roi_map = [] % figure out map of fmri voxels to map
+        roi_sigmas = []
         roi_voxels = 1000 % figure out count of voxels in ROI
         rfi_data
         last_vol_mc_in_proc
         last_vol_mc_proc
         run_bool = true
         ZSCORE_VOLS = 10
+        mc_optimizer
+        mc_metric
     end
 
     methods
@@ -33,26 +36,23 @@ classdef testWatcher < handle
             self.archive_dir = strcat(self.watch_dir,'archive');
             self.resetForNextRun
             self.rfi_data = zeros(self.num_slices, self.slice_dim, self.slice_dim); % load from ref
+            [self.mc_optimizer, self.mc_metric] = imregconfig('monomodal');
         end
 
-        function resetForNextRun
-            fmri_data = zeros(self.volumes_per_run,self.num_slices,self.slice_dim^2);
-            fmri_data_mc = zeros(self.volumes_per_run,self.num_slices,self.slice_dim^2);
-            fmri_data_raw_status = zeros(self.volumes_per_run,self.num_slices);
-            fmri_data_mc_status = zeros(self.volumes_per_run,self.num_slices);
-            last_vol_mc_in_proc = 1;
-            last_vol_mc_proc = 1;
+        function resetForNextRun(self)
+            self.fmri_data = zeros(self.volumes_per_run,self.num_slices,self.slice_dim^2);
+            self.fmri_data_mc = zeros(self.volumes_per_run,self.num_slices,self.slice_dim^2);
+            self.fmri_data_raw_status = zeros(self.volumes_per_run,self.num_slices);
+            self.last_vol_mc_in_proc = 1;
+            self.last_vol_mc_proc = 1;
         end
 
         function update(self)
-            while self.run_bool
-                self.checkForSlices;
-                % self.checkSliceStatus; % add if need to async slice read
-                % self.processNewSlices; % add if need to async slice read
-                self.checkVolumeStatus;
-                self.processNewVolume;
-                self.archiveOldVolumes;
-            end
+            self.checkForSlices;
+            % self.checkSliceStatus; % add if need to async slice read
+            % self.processNewSlices; % add if need to async slice read
+            self.checkVolumeStatus;
+            self.archiveOldVolumes;
         end
 
         function checkForSlices(self)
@@ -79,41 +79,48 @@ classdef testWatcher < handle
 
         function checkVolumeStatus(self)
             % do motion correction
-            while all(self.fmri_data_raw_status(:,self.last_vol_mc_in_proc) == 1)
-                eval_handle = parfeval(self.pool, @self.motionCorrectVolume, 2,...
-                    self.last_vol_mc_in_proc,...
-                    self.fmri_data(self.last_vol_mc_in_proc,:,:),...
-                    self.rfi_data,...
-                    self.slice_dim);
+            while (all(self.fmri_data_raw_status(self.last_vol_mc_in_proc,:) == 1) && (self.last_vol_mc_in_proc <= self.volumes_per_run))
+                tic
+                eval_handle = parfeval(self.pool, @imregister, 2,...
+                    reshape(self.fmri_data(self.last_vol_mc_in_proc,:,:),self.num_slices,self.slice_dim,self.slice_dim),...
+                    self.rfi_data,'rigid', self.mc_optimizer, self.mc_metric);
+                toc
                 self.mc_proc_handles = [self.mc_proc_handles eval_handle];
                 self.last_vol_mc_in_proc = self.last_vol_mc_in_proc + 1;
             end
             % grab motion corrected data
-            if self.mc_proc_handles(1).State == 'finished'
-                mc_vol = fetchOutputs(self.mc_proc_handles(1));
-                self.fmri_data_mc(self.last_vol_mc_proc,:,:) = mc_vol;
-                self.last_vol_mc_proc = self.last_vol_mc_proc + 1;
-                self.mc_proc_handles(1) = [];
+            if length(self.mc_proc_handles>0)
+                if strcmp(self.mc_proc_handles(1).State,'finished')
+                    mc_vol = fetchOutputs(self.mc_proc_handles(1));
+                    self.fmri_data_mc(self.last_vol_mc_proc,:,:) = reshape(mc_vol,self.num_slices,self.slice_dim^2);
+                    self.last_vol_mc_proc = self.last_vol_mc_proc + 1;
+                    self.mc_proc_handles(1) = [];
+                end
             end
 
-            if self.last_vol_mc_proc < self.ZSCORE_VOLS
-                % do nothing
-            elseif self.last_vol_mc_proc == self.ZSCORE_VOLS
-                % calc zscore
-            else
-                % calc linear detrend and applyClassifier(noise_var, mean, linear_trend)
+            if self.last_vol_mc_proc == self.ZSCORE_VOLS
+                self.zScoreCalc;
+            end
+
+            if self.last_vol_mc_proc >= self.ZSCORE_VOLS
+                self.classifierCalc(self.last_vol_mc_proc);
             end
             % check self.volume_processing_status
         end
 
         function mc_vol = motionCorrectVolume(self,...
-                fmri_data, rfi_data, slice_dim)
-            % pause(.5);
-            mc_vol = fmri_data;
+                fmri_data, rfi_data)
+            mc_vol = imregister(fmri_data, rfi_data, 'rigid', self.mc_optimizer, self.mc_metric);
         end
 
-        function z_scored_roi = zScoreRoi(self, roi_data_mc)
-            % code
+        function zScoreCalc(self)
+            % self.fmri_data_mc in roi
+            % calculate sigmas
+        end
+
+        function classifierCalc(self, volume)
+            % only within ROI
+            % calc linear detrend, apply zscore, and applyClassifier(noise_var, mean, linear_trend)
         end
 
         function archiveOldVolumes(self)
@@ -140,6 +147,12 @@ classdef testWatcher < handle
             vis_slice = vis_slice/max(max(vis_slice));
             imshow(vis_slice);
         end
+
+        function showSliceMc(self, rep, slice)
+            vis_slice = reshape(self.fmri_data_mc(rep,slice,:),self.slice_dim,self.slice_dim);
+            vis_slice = vis_slice/max(max(vis_slice));
+            imshow(vis_slice);
+        end
     end
 
     methods(Static)
@@ -148,10 +161,7 @@ classdef testWatcher < handle
             slice_data = fread(f,'uint16');
             fclose(f);
         end
-        % 4. after processing:
-        %    - move to watch_dir/processed
-        %    - remove from in_process list
-        %    - increment dict/counter
+
         function roi_voxels = mapVoxelsToRoi(voxels, roi_map)
             roi_voxels = voxels*roi_map;
         end
