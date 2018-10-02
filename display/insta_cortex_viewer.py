0602f0db02f95d34301dfad2b4a1d7e776608eec
efunn <e@ethanoblak.com>
2018-10-01 21:35:02 -0500

    colormap options added for pycortex viewer

--- display/insta_cortex_viewer.py
+++ display/insta_cortex_viewer.py
@@ -4,23 +4,22 @@ import nibabel as nib
 import time
 
 class InstaCortexViewer(object):
-    # bufferlen = 50
-    bufferlen = 2
-
     def __init__(self, subject, xfm_name, html_template='simple.html', port=4567,
-                 mask_type='thick', vmin=-1., vmax=1., **kwargs):
+                 mask_type='thick', cmap='fingfind_heatmap_2', vmin=-1., vmax=1., **kwargs):
         super(InstaCortexViewer, self).__init__()
         npts = cortex.db.get_mask(subject, xfm_name, mask_type).sum()
 
+        self.bufferlen = 2
         data = np.zeros((self.bufferlen, npts), 'float32')
-        vol = cortex.Volume(data, subject, xfm_name, cmap='fingfind_heatmap_2', vmin=vmin, vmax=vmax)
-        # vol = cortex.Volume(data, subject, xfm_name, vmin=vmin, vmax=vmax)
+        vol = cortex.Volume(data, subject, xfm_name, cmap=cmap, vmin=vmin, vmax=vmax)
         view = cortex.webshow(vol, port=port, autoclose=False, template=html_template, title='instabrain')
 
         self.subject = subject
         self.xfm_name = xfm_name
         self.mask_type = mask_type
+        # self.functional_dir = '/Users/efun/Dropbox/pycortex-db/subjects/'+self.subject+'/functionals'
         self.functional_dir = '/Users/eo5629/fmri/'+self.subject+'/ref'
+        # self.functional_dir = '/Users/efun/Dropbox/pycortex-db/subjects/'+self.subject+'/functionals'
 
         self.view = view
         self.active = True
@@ -37,7 +36,6 @@ class InstaCortexViewer(object):
 
     def load_nib_to_mos(self, img_name):
         img_dir = self.functional_dir+'/'+img_name+'.nii'
-        # data = np.array(nib.load(img_dir).get_data().swapaxes(0,2),'float32')+7500*np.random.random((36,100,100))
         data = np.array(nib.load(img_dir).get_data().swapaxes(0,2),'float32')
         vol = cortex.Volume(data, self.subject, self.xfm_name)
         mos, _ = cortex.mosaic(vol.volume[0], show=False)
@@ -85,6 +83,6 @@ class InstaCortexViewer(object):
 if __name__ == '__main__':
     subj_id = 'ff001'
     xfm_name = 'rai2rfi'
-    viewer = InstaCortexViewer(subj_id, xfm_name) #, port=4567)
-    print 'Instabrain started!'
+    viewer = InstaCortexViewer(subj_id, xfm_name)
+    print 'PyCortex started!'
     while True: pass
